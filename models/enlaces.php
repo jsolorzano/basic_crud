36de5d53272d314f9202b2fa708843427ef71f0c
José Solorzano <solorzano202009@gmail.com>
2020-04-17 22:03:47 -0400

    Primera funcionalidad para el módulo de ingreso.

--- models/enlaces.php
+++ models/enlaces.php
@@ -15,6 +15,10 @@ class Paginas{
 		
 			$module = "views/modules/registro.php";
 		
+		}else if($enlaces == "fallo"){
+		
+			$module = "views/modules/ingresar.php";
+		
 		}else{
 		
 			$module = "views/modules/registro.php";
--- views/modules/ingresar.php
+++ views/modules/ingresar.php
@@ -1,11 +1,28 @@
 <h1>INGRESAR</h1>
 
-	<form method="post" action="">
-		
-		<input type="text" placeholder="Usuario" name="usuario" required>
+<form method="post" action="">
+	
+	<input type="text" placeholder="Usuario" name="usuarioIngreso" required>
 
-		<input type="password" placeholder="Contraseña" name="password" required>
+	<input type="password" placeholder="Contraseña" name="passwordIngreso" required>
 
-		<input type="submit" value="Enviar">
+	<input type="submit" value="Enviar">
 
-	</form>
+</form>
+
+<?php
+
+$ingreso = new MvcController();
+$ingreso->ingresoUsuarioController();
+
+if(isset($_GET['action'])){
+	
+	if($_GET['action'] == "fallo"){
+	
+		echo "Fallo al ingresar";
+	
+	}
+	
+}
+
+?>
